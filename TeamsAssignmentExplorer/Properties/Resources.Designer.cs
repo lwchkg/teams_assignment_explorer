0f44c7b08ed927bee0c6a77af904e1fb9d5c21ea
WC Leung <lwchkg@gmail.com>
2020-09-20 18:07:55 +0800

    Fix assembly name, and some small cleanup

--- TeamsAssignmentExplorer/Properties/Resources.Designer.cs
+++ TeamsAssignmentExplorer/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace TeamsAssignmentExplorer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("StuWorkExplorer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TeamsAssignmentExplorer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
