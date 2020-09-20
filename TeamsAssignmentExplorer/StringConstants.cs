d4e94657a7aa784c0432733619f7d476cefebe04
WC Leung <lwchkg@gmail.com>
2020-09-21 01:52:57 +0800

    Update interface: WIP
    Changed font and font size: Arial Narrow 21
    Added icons for opening file and parent folder.
    Todo: clip text but not icon if text is too long.

---
+++ TeamsAssignmentExplorer/StringConstants.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamsAssignmentExplorer
+{
+    class StringConstants
+    {
+        public const string submittedFiles = "Submitted files";
+        public const string workingFiles = "Working files";
+    }
+}
