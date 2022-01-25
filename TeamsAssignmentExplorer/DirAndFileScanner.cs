3492e2dffc6eeb6e4644f2847b9d73292fdbc664
WC Leung <lwchkg@gmail.com>
2022-01-25 14:43:32 +0800

    Modify detection algorithm of submitted files
    Now [basePath]/Submitted files/[user]/[homework]/*.* can be identified
    as submitted files in some cases.
    
    Consider the current file paths in Teams assignment, this allow
    detection of JPEG files (no "Version #" in path) provided that another
    student submits a PDF file (has "Version #" in path).

--- TeamsAssignmentExplorer/DirAndFileScanner.cs
+++ TeamsAssignmentExplorer/DirAndFileScanner.cs
@@ -95,19 +95,27 @@ namespace TeamsAssignmentExplorer
         {
             System.Diagnostics.Debug.Assert(homework.Trim() != "");
 
-            // Glob [basePath]/Submitted files/[user]/[homework]/Version */*.*
-            // and [basePath]/Working files/[user]/[homework]/Version */*.*
+            // Glob [basePath]/Submitted files/[user]/[homework]/Version */*.*,
+            // [basePath]/Submitted files/[user]/[homework]/*.*,
+            // and [basePath]/Working files/[user]/[homework]/*.*
             //
             // Due to translations of folder names by Microsoft, we do not check for the phrases
             // "Submitted files", "Working files" and "Version". Otherwise we need to add code for
-            // every translation.)
+            // every translation.
+            //
+            // There is no failproof way to identify the second type of file as submitted files, but
+            // we do it with best effort.
 #if DEBUG
             var stopwatch = new System.Diagnostics.Stopwatch();
             stopwatch.Start();
 #endif
+            // We opt not to persist the names of submitted file folders across searches, because
+            // users can carelessly moving files to other folder. Note that this only affect this
+            // app, but not MS Teams as they identify files by UUID.
+            var submittedFilesDirNames = new ConcurrentDictionary<string, bool>();
 
             var submittedFiles = new ConcurrentBag<string>();
-            var workingFiles = new ConcurrentBag<string>();
+            var maybeWorkingFiles = new ConcurrentBag<string>();
             try
             {
                 foreach (string submittedOrWorkingFilesDir in
@@ -116,25 +124,45 @@ namespace TeamsAssignmentExplorer
                     Parallel.ForEach(Directory.EnumerateDirectories(submittedOrWorkingFilesDir),
                         userDir =>
                         {
+                            bool isSubmittedFilesDir = false;
+
                             string hwPath = Path.Combine(userDir, homework);
                             if (!Directory.Exists(hwPath)) return;
 
                             foreach (string versionDir in Directory.EnumerateDirectories(hwPath))
                             {
                                 foreach (string file in Directory.EnumerateFiles(versionDir))
+                                {
                                     submittedFiles.Add(StripBasePath(file, basePath));
+                                    isSubmittedFilesDir = true;
+                                }
+                            }
+
+                            if (isSubmittedFilesDir)
+                            {
+                                submittedFilesDirNames.TryAdd(
+                                    Path.GetFileName(submittedOrWorkingFilesDir), true);
                             }
 
                             foreach (string file in Directory.EnumerateFiles(hwPath))
-                                workingFiles.Add(StripBasePath(file, basePath));
+                                maybeWorkingFiles.Add(StripBasePath(file, basePath));
                         });
                 }
             }
             catch (Exception) { /* Do nothing */ }
 
             List<string> submittedFilesSorted = submittedFiles.ToList();
+            List<string> workingFilesSorted = new List<string>();
+
+            foreach (string file in maybeWorkingFiles)
+            {
+                string topDir = file.Substring(0, file.IndexOf(Path.DirectorySeparatorChar));
+                if (submittedFilesDirNames.ContainsKey(topDir))
+                    submittedFilesSorted.Add(file);
+                else
+                    workingFilesSorted.Add(file);
+            }
             submittedFilesSorted.Sort();
-            List<string> workingFilesSorted = workingFiles.ToList();
             workingFilesSorted.Sort();
 
 #if DEBUG
